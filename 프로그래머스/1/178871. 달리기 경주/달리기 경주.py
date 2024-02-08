136c882bf7d6c2cb0ac723f0b556e85219dce7b0
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-02-08 10:58:25 +0900

    [level 1] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,20 @@
+def solution(players, callings):
+    answer = []
+    thr = ""
+    score= {}
+    for i in range(len(players)):
+        score[players[i]] = i
+
+    for name in callings:
+        n = score[name]
+        score[players[n-1]] += 1
+        score[name] -= 1
+        
+        e = players[n-1]
+        players[n-1] = players[n]
+        players[n] = e
+        
+        
+    score = sorted(score.items(), key=lambda x: x[1])
+
+    return [item[0] for item in score]
