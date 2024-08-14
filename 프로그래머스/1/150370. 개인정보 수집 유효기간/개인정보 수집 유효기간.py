f8e653b78c97e61f7c1ba6caa828ebec6a11a878
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-08-14 15:59:48 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.37 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,28 @@
+def solution(today, terms, privacies):
+    answer = []
+    terms_dic = {}
+    for term in terms:
+        product,month = term.split(" ")
+        terms_dic[product] = month
+    for i in range(len(privacies)):
+        date,target = privacies[i].split(" ")
+        year,mon,day = map(int,date.split("."))
+        mon += int(terms_dic[target])
+        if mon > 12:
+            year += (mon//12)
+            mon%=12
+            
+        if mon == 0:
+            year -= 1
+            mon = 12
+        day -= 1
+        today_year,today_mon,today_day = map(int,today.split("."))
+        if today_year > year:
+            answer.append(i+1)
+        elif today_year == year:
+            if today_mon > mon:
+                answer.append(i+1)
+            elif today_mon == mon:
+                if today_day > day :
+                    answer.append(i+1)
+    return answer
