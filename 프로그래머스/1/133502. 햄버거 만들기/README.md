b7f43dbfb8d51a81e909d4419e552339dd985afc
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-08-14 15:22:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 196.36 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 26.7 MB, 시간: 272.94 ms
+메모리: 26.8 MB, 시간: 196.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 14일 15:21:25
+2024년 08월 14일 15:21:58
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,14 +1,21 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
 def solution(ingredient):
-    answer = 0
-    stack = []
-    
-    for ing in ingredient:
-        stack.append(ing)
-        
-        # 스택의 마지막 4개의 요소가 [1,2,3,1]인지 확인
-        if len(stack) >= 4 and stack[-4:] == [1, 2, 3, 1]:
-            # 패턴이 발견되면 스택에서 제거
-            stack[-4:] = []  # 슬라이싱을 통해 한 번에 제거
-            answer += 1
-    
-    return answer
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            for i in range(4):
+                s.pop()
+    return cnt
