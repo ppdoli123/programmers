4e337d65698bb3e4e20ece46142dff93c82ab412
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-10-13 00:22:00 +0900

    [level 2] Title: 후보키, Time: 13.14 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -15,7 +15,27 @@ def solution(relation):
         temp = [tuple(relation[row][col] for col in combo) for row in range(rows)]
         if len(set(temp)) == rows:
             unique_combinations.append(combo)
-
+    print(unique_combinations)
+    uni = [i for i in range(columns)]
+    rows = len(relation)
+    final = []
+    for i in range(1,columns+1):
+        cur = list(itertools.combinations(uni,i))
+        for c in cur:
+            del_uni = []
+            unique = []
+            for index in range(columns):
+                if index in c:
+                    for row in range(rows): 
+                        unique.append([relation[row][index]])
+                    del_uni.append(index)
+            for col in range(len(unique)):
+                if col%rows != col:
+                    unique[col%rows] += unique[col]
+            unique = unique[:rows]
+            if len(list(set([tuple(set(uniq[:rows])) for uniq in unique]))) == len(unique):
+                final.append(del_uni)
+    print(final)
     # 최소성 체크
     candidate_keys = []
     for combo in unique_combinations:
