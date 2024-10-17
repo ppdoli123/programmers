06ea51fd009fcc09b69744311e87de3e6323dfc3
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-10-17 15:21:43 +0900

    [level 3] Title: 네트워크, Time: 9.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.52 ms
+메모리: 10.3 MB, 시간: 9.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 23:47:23
+2024년 10월 17일 15:21:41
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,61 @@
-def solution(n, computers):
-    def dfs(node, visited, computers):
-        stack = [node]
-        while stack:
-            current = stack.pop()
-            for neighbor in range(n):
-                if computers[current][neighbor] == 1 and not visited[neighbor]:
-                    visited[neighbor] = True
-                    stack.append(neighbor)
-
-    visited = [False] * n
-    network_count = 0
-
+from collections import deque
+def solution(n,computers):
+    visited = [True]*n
+    answer = 0
+    aa = [0]
+    que = deque()
     for i in range(n):
-        if not visited[i]:
-            dfs(i, visited, computers)
-            network_count += 1
+        stack = []
+        if visited[i]:
+            for j in range(i,n):
+                if computers[i][j] == 1:
+                    que.append([i,j])
+                    visited[i] = False 
+                    stack.append(i)
+            while que:
+                x,y = que.popleft()
+                stack.append(y)
 
-    return network_count
+                for x in range(n):
+                    if  computers[y][x] == 1 and visited[x] and [x,y] not in que:
+                        que.append([y,x])
+                        computers[y][x] = 0
+            if len(set(stack)) != 0 :
+                answer +=1
+    return answer
+#     def dfs(i,visited,ans):
+#         if 1 in computers[i][i+1:]:
+#             for x in range(i+1,n):
+#                 if computers[i][x] == 1 and visited[x]:
+#                     visited[x] = False
+#                     ans.append(x)
+#                     dfs(x,visited,ans)
+#         else:
+#             aa[0] += 1
+#             return aa[0]
+#         print(i,visited,aa[0],ans)
+        
+#     for i in range(n):
+#         if visited[i]:
+#             ans = [i]
+#             dfs(i,visited,ans)
+#             print(ans)
+# def solution(n, computers):
+#     def dfs(node, visited, computers):
+#         stack = [node]
+#         while stack:
+#             current = stack.pop()
+#             for neighbor in range(n):
+#                 if computers[current][neighbor] == 1 and not visited[neighbor]:
+#                     visited[neighbor] = True
+#                     stack.append(neighbor)
+
+#     visited = [False] * n
+#     network_count = 0
+
+#     for i in range(n):
+#         if not visited[i]:
+#             dfs(i, visited, computers)
+#             network_count += 1
+
+#     return network_count
