1bce5d420d28564e4c66a733d199fb7efced57d8
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-10-18 16:08:38 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,16 +1,13 @@
--- 코드를 입력하세요
-with base as (SELECT distinct(c.car_id), c.car_type,c.daily_fee  from car_rental_company_car c join car_rental_company_rental_history h  on c.car_id = h.car_id 
-             where c.car_id not in ( select distinct(h.car_id) from car_rental_company_rental_history h where datediff(h.end_date,'2022-11-01') >= 0))
+# -- 코드를 입력하세요
+# with base as (SELECT distinct(c.car_id), c.car_type,c.daily_fee  from car_rental_company_car c join car_rental_company_rental_history h  on c.car_id = h.car_id 
+#              where c.car_id not in ( select c.car_id from car_rental_company_rental_history where datediff(h.end_date,'2022-11-01') >= 0 )) 
 
-# with base as ( select distinct(h.car_id) from car_rental_company_rental_history h where datediff(h.end_date,'2022-11-01') >= 0)
-
-select base.car_id, base.car_type ,round((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) as fee from car_rental_company_discount_plan p join base on base.car_type = p.car_type
-where p.duration_type regexp "(^30)" and p.car_type regexp ("^세단|^suv") 
-and ((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) >= 500000 and ((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) < 2000000
-order by 3 desc, 2 , 1 desc
-
-
-
+# select base.car_id, base.car_type ,round((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) as fee from car_rental_company_discount_plan p join base on base.car_type = p.car_type
+# where p.duration_type regexp "(^30)" and p.car_type regexp ("^세단|^suv") 
+# and ((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) >= 500000 and ((100-p.discount_rate) * base.daily_fee * 0.01 * 30 ) < 2000000
+# order by 3 desc, 2 , 1 desc
+select distinct(c.car_id), h.history_id , h.start_date,h.end_date from car_rental_company_car c join car_rental_company_rental_history h on c.car_id = h.car_id
+order by 4 desc
 # SELECT 
 #     C.CAR_ID, 
 #     C.CAR_TYPE, 
