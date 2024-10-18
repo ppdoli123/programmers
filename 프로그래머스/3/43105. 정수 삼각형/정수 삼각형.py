798eff4c5501aa78d9f278c32037e1e9f793453e
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2024-10-18 17:47:22 +0900

    [level 3] Title: 정수 삼각형, Time: 47.40 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(triangle):
+    answer = 0
+    que = deque()
+    maxv = []
+    dp = [[0] * i for i in range(1,len(triangle)+1)]
+    dp[0][0] = triangle[0][0]
+    for i in range(1,len(triangle)):
+
+        for j in range(i+1):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+
+    return max(dp[-1])
