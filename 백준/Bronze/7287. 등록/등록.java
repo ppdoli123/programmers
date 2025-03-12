cf64b85b014dc7734f436d22ab3ae3079d3ccdf8
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2025-03-12 16:58:50 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("7");
+        System.out.println("ppdoli123");
+    }
+}
