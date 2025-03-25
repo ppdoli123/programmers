6f90d004e164b9eb0c80e31e002bc6572a8c53f0
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2025-03-26 08:42:32 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+        //아래 예제와 같이 고양이를 출력하시오.
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+        
+    }
+
+    }
