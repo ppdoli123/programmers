cff549e752ccf32cfaa282de2445fa43810229da
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2025-04-11 08:34:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 프로그램 시작 지점
+        Scanner sc = new Scanner(System.in);
+        char ascil = sc.next().charAt(0);
+        System.out.println((int) ascil);
+    }
+}
