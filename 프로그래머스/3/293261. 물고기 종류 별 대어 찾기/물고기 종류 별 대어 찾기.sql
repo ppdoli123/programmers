bf9592c844d1ddea1ee226910b52f3813bf72cb5
SeungHyun <93701796+ppdoli123@users.noreply.github.com>
2025-06-11 09:31:25 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
-SELECT A.ID,B.FISH_NAME,A.LENGTH
+SELECT DISTINCT A.ID,B.FISH_NAME,A.LENGTH
 FROM FISH_INFO AS A 
 JOIN FISH_NAME_INFO AS B 
 ON A.FISH_TYPE = B.FISH_TYPE
@@ -7,3 +7,4 @@ WHERE (B.FISH_TYPE , A.LENGTH) in
 (select fish_type,max(length)
 from fish_info 
 group by fish_type);
+
